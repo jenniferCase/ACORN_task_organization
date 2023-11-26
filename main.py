ed3ffe890dec9d2a85df7324ebf81cca055ab567
awhitbeck <whitbeck.andrew@gmail.com>
2023-11-26 14:25:17 -0800

    I make Task a superset of the data members of Task, Labor, and Acquisition.   This is really a test bed.  It is not clear if this is better, but it does make some things a little easier.  For example, you don't need two different formats, and if you need to apply transformation on the superset of Labor and Acquisition objects, you don't need to worry about know which is which type if you just work from the base class.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # class that describes a task
 class Task:
-    def __init__(self, name, description):
+    def __init__(self, name, description, is_acquisition=False):
         self.name = name
         self.description = description
         # a task should have a name, a total_cost, a duration, can be a mileston or not,
@@ -14,14 +14,25 @@ class Task:
         self.dependencies = []
         self.parent = None
         self.children = []
+        self.is_acquisition = is_acquisition
+        # an acquisition task should have a unit price, a quantity, percent spares
+        self.unit_price = 0
+        self.quantity = 0
+        self.percent_spares = 0
+        # a labor task should have a rate, and a number of hours
+        self.rate = 0
+        self.hours = 0
+        # a mapping of the csv file columns to the object attributes
+        # below are the mapping used by this class for exporting to CSV
+        self.mapping = {"name":0,"description":1,"total_cost":2,"duration":3,"is_milestone":4,"is_deliverable":5,"parent":6,"is_acquisition":7,"unit_price":8,"quantity":9,"percent_spares":10,"rate":11,"hours":12,"dependencies":13}
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name}:{self.description}"
 
     def to_csv(self):
         if self.dependencies == []: dependencies = ""
         else: dependencies = self.dependencies[0]+":".join(self.dependencies[1:])
-        return f"{self.name},{self.description},{self.total_cost},{self.duration},{self.is_milestone},{self.is_deliverable},{dependencies},{self.parent}"
+        return f"{self.name},{self.description},{self.total_cost},{self.duration},{self.is_milestone},{self.is_deliverable},{self.parent},{self.is_acquisition},{self.unit_price},{self.quantity},{self.percent_spares},{self.rate},{self.hours},{dependencies}"
 
     def print(self):
         print("--------------------")
@@ -33,48 +44,17 @@ class Task:
         print("dependencies: ","".join(list(map(str,self.dependencies))))
         print("parent: ",self.parent)
         print("children: ","".join(list(map(str,self.children))))
+        print("is acquisition: ",self.is_acquisition)
+        print("--------------------")
+        print("unit price: ", self.unit_price)
+        print("quantity: ", self.quantity)
+        print("percent spares: ", self.percent_spares)
+        print("--------------------")
+        print("rate: ", self.rate)
+        print("hours: ", self.hours)
 
-# create a class, Acquisition, that inherits from Task
-class Acquisition(Task):
-    def __init__(self, name, description):
-        super().__init__(name, description)
-        # an acquisition should have a unit price, a quantity, percent spares
-        self.unit_price = 0
-        self.quantity = 0
-        self.percent_spares = 0
-
-    def to_csv(self):
-        return f"{super().to_csv()},{self.unit_price},{self.quantity},{self.percent_spares}"
-
-    def __str__(self):
-        return f"Acquisition Task[{self.name}: {self.description}]"
-
-    def print(self):
-        print("Acquisition Task ----------")
-        super().print()
-        print("unit price: ",self.unit_price)
-        print("quantity: ",self.quantity)
-        print("percent spares: ",self.percent_spares)
-
-# create a class, Labor, that inherits from Task
-class Labor(Task):
-    def __init__(self, name, description):
-        super().__init__(name, description)
-        # a labor should have a rate, and a number of hours
-        self.rate = 0
-        self.hours = 0
-
-    def to_csv(self):
-        return f"{super().to_csv()},,{self.rate},{self.hours}"
-
-    def __str__(self):
-        return f"Labor Task[{self.name}: {self.description}]"
-
-    def print(self):
-        print("Labor Task ----------------")
-        super().print()
-        print("rate: ",self.rate)
-        print("hours: ",self.hours)
+    def get_total_cost(self):
+        return self.total_cost
 
 if __name__ == '__main__':
     t1 = Task("task1","description1");
@@ -82,7 +62,8 @@ if __name__ == '__main__':
     t1.total_cost = 100;
     t1.is_milestone = True;
     t1.is_deliverable = False;
-    t2 = Acquisition("task2","description2");
+    t2 = Task("task2","description2");
+    t2.is_acquisition=True;
     t2.duration = 5;
     t2.total_cost = 50;
     t2.is_milestone = False;
@@ -92,7 +73,7 @@ if __name__ == '__main__':
     t2.quantity = 10;
     t2.percent_spares = 0.1;
 
-    t3 = Labor("task3","description3");
+    t3 = Task("task3","description3",False);
     t3.duration = 5;
     t3.total_cost = 50;
     t3.is_milestone = False;
